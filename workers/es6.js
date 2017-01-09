67efa7a6ed94951fb81fc73fc66f62df1c69b6e5
Jarrett Cruger <jcrugzz@gmail.com>
2017-01-09 18:41:17 -0500

    initial transfer

---
+++ workers/es6.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Factory = require('../factory');
+const babel = require('babel-core');
+const path = require('path');
+
+/**
+ * Setup factory line.
+ *
+ * @param {Object} data Builder options, package location etc.
+ * @api public
+ */
+module.exports = function bab(data, callback) {
+  const factory = new Factory(data, run);
+
+  factory.line([
+    factory.unpack,
+    factory.init,
+    factory.exists,
+    factory.read,
+    factory.assemble,
+    factory.minify,
+    factory.pack,
+    factory.clean
+  ], callback);
+
+  return factory;
+};
+
+
+/**
+ * Execute es6 build.
+ *
+ * @param {Function} next Completion callback
+ * @api public
+ */
+module.exports.run = function run(next) {
+  const output = {};
+
+  output[path.basename(this.entry)] = babel.transform(this.source, {
+    presets: ['babel-preset-es2015', 'babel-preset-react']
+  }).code;
+
+  next(null, output);
+};
