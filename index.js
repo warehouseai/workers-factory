fbe18ab4b37163bba8291612b71e5abeda2b9873
Jarrett Cruger <jcrugzz@gmail.com>
2017-01-09 18:41:17 -0500

    initial transfer

---
+++ index.js
@@ -0,0 +1,6 @@
+
+module.exports.webpack = require('./workers/webpack');
+module.exports.es6 = require('./workers/es6');
+module.exports.browserify = require('./workers/browserify');
+module.exports = require('./factory');
+module.exports.Factory = require('./factory');
