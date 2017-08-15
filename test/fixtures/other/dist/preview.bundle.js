2dee9b9226c4ed02217c88f7467f0df296734bd0
Erica Pramer <epramer@godaddy.com>
2017-08-15 11:23:12 -0700

    dont modify dist

--- test/fixtures/other/dist/preview.bundle.js
+++ test/fixtures/other/dist/preview.bundle.js
@@ -2,49 +2,49 @@ webpackJsonp([1],[
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-  'use strict';
+	'use strict';
 
-  Object.defineProperty(exports, '__esModule', {
-    value: true
-  });
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
 
-  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-  var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-  function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-  var _react = __webpack_require__(1);
+	var _react = __webpack_require__(1);
 
-  var _sum = __webpack_require__(2);
+	var _sum = __webpack_require__(2);
 
-  var Test = (function (_React$Component) {
-    _inherits(Test, _React$Component);
+	var Test = (function (_React$Component) {
+	  _inherits(Test, _React$Component);
 
-    function Test() {
-      _classCallCheck(this, Test);
+	  function Test() {
+	    _classCallCheck(this, Test);
 
-      _get(Object.getPrototypeOf(Test.prototype), 'constructor', this).apply(this, arguments);
-    }
+	    _get(Object.getPrototypeOf(Test.prototype), 'constructor', this).apply(this, arguments);
+	  }
 
-    _createClass(Test, [{
-      key: 'render',
-      value: function render() {
-        return _react.React.createElement(
-          'p',
-          null,
-          'Preview ES6 React component.'
-        );
-      }
-    }]);
+	  _createClass(Test, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react.React.createElement(
+	        'p',
+	        null,
+	        'Preview ES6 React component.'
+	      );
+	    }
+	  }]);
 
-    return Test;
-  })(_react.React.Component);
+	  return Test;
+	})(_react.React.Component);
 
-  exports['default'] = Test;
-  module.exports = exports['default'];
+	exports['default'] = Test;
+	module.exports = exports['default'];
 
 /***/ }
 ]);
